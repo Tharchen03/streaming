2f46dc08f758b75ab60b578d4fc25f49578c7128
Tharchen03 <sherabten098@gmail.com>
2024-07-03 10:53:18 +0600

    up

--- layout/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>
-        &copy; 2024 Streaming| Design by:
-        <a href="https://ibest.com" target="_blank">iBEST TECHONOLOGIES</a>
-    </p>
-</footer>
--- layout/nav.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <div class="navM">
-        <i class="icon-credit-card"></i>
-        <h1>Vdocipher</h1>
-    </div>
-</nav>
